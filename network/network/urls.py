40337f5f71b479c18094b391e1cecb3000a6c22a
stephen-w-choo <stephen.w.choo@gmail.com>
2022-10-13 17:49:39 +1100

    Finally got following to work properly

--- network/network/urls.py
+++ network/network/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("make_post", views.make_post, name="make_post"),
     path("profile/<int:profile_id>", views.profile, name = "profile"),
     path("follow/<int:profile_id>", views.follow, name = "follow"),
+    path("following", views.following, name = "following")
 ]
